96be247e0d1e7984330c884f022752f460ef0d85
Mishail <93082995+11mishanik11@users.noreply.github.com>
2023-03-05 00:00:45 +0300

    slider-clients

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,89 @@
 // Бургер 
-let burgerBtn = document.querySelector('#burger-btn')
-let headerMenu = document.querySelector('#header-menu')
-
-burgerBtn.onclick = function() {
-    if(headerMenu.classList.contains('open')) {
-        menuHide()
+(function() {
+    let burgerBtn = document.querySelector('#burger-btn')
+    let headerMenu = document.querySelector('#header-menu')
+    
+    burgerBtn.onclick = function() {
+        if(headerMenu.classList.contains('open')) {
+            menuHide()
+        }
+        else {
+            menuShow()
+        }
     }
-    else {
-        menuShow()
+    
+    document.onclick = function(e) {
+        if(!(e.composedPath().includes(burgerBtn) || e.composedPath().includes(headerMenu))) {
+            menuHide()
+        } 
     }
-}
-
-document.onclick = function(e) {
-    if(!(e.composedPath().includes(burgerBtn) || e.composedPath().includes(headerMenu))) {
-        menuHide()
-    } 
-}
-
-function menuShow() {
-    headerMenu.classList.add('open')
-    burgerBtn.classList.add('open')
-
-    burgerBtn.setAttribute('aria-expanded', 'true')
-    headerMenu.setAttribute('aria-hidden', 'false')
-}
-
-function menuHide() {
-    headerMenu.classList.remove('open')
-    burgerBtn.classList.remove('open')
-
-    burgerBtn.setAttribute('aria-expanded', 'false')
-    headerMenu.setAttribute('aria-hidden', 'true')
-}
+    
+    function menuShow() {
+        headerMenu.classList.add('open')
+        burgerBtn.classList.add('open')
+    
+        burgerBtn.setAttribute('aria-expanded', 'true')
+        headerMenu.setAttribute('aria-hidden', 'false')
+    }
+    
+    function menuHide() {
+        headerMenu.classList.remove('open')
+        burgerBtn.classList.remove('open')
+    
+        burgerBtn.setAttribute('aria-expanded', 'false')
+        headerMenu.setAttribute('aria-hidden', 'true')
+    }
+})()
 
 
+
+// Превью слайдера клиентов
+const clientsThumb = new Swiper(".thumb-clients", {
+
+    direction: 'vertical',
+
+    navigation: {
+        nextEl: '.thumb-clients__next',
+        prevEl: '.thumb-clients__prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          direction: 'horizontal',
+        },
+        360: {
+            slidesPerView: 3,
+            direction: 'horizontal',
+          },
+        992: {
+            slidesPerView: 4,
+            direction: 'horizontal',
+        },
+        1140: {
+            slidesPerView: 4,
+            direction: 'vertical',
+        }
+    }
+})
 
 // Слайдер клиентов
-// Прогресс бар
-const circle = document.querySelector('.mini-clients__circle');
-const r = circle.r.baseVal.value;
-const circumference = 2 * Math.PI * r;
+const clientsCarousel = new Swiper(".carousel-clients", {
+    slidesPerView: 1,
+    effect: 'fade',
 
-circle.style.strokeDasharray = `${circumference} ${circumference}`;
-circle.style.strokeDashoffset = circumference;
-
-function setProgress(percent) {
-  const offset = circumference - percent / 100 * circumference;
-  circle.style.strokeDashoffset = offset;
-}
-
-setProgress(100);
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    thumbs: {
+        swiper: clientsThumb,
+    },
+})
 
 
-
-
-// Инит слайдра
+// Инит слайдра сертификатов
 const diplomasSwiper = new Swiper(".diplomas__swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
